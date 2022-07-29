cb39e3a9a1bc71e0d28267e22d5b5af91035614e
18133870418 <1172969610@qq.com>
2022-07-30 02:30:47 +0800

    github page setting

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你要部署在 https://<USERNAME>.github.io
-git push -f git@github.com:XAihan/XAihan.github.io.git main
+git push -f git@github.com:XAihan/ScrollerNumber.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:XAihan/ScrollerNumber.github.io.git main:gh-pages
 
 cd -
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/scrollerNumber',
   server: {
     host: '127.0.0.1'
     // host: '10.205.97.53'
