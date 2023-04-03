daa61ca78c801d10e0cb17e93c740a6a42e2d2fe
Inkstone <1172969610@qq.cpm>
2023-04-03 17:45:17 +0800

    进行github page的提交和部署

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,17 @@ set -e
 # 构建
 npm run build
 
-# 进入构建文件夹
-cd dist
-
 # 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
+git add . 
+git commit -m "进行github page的提交和部署"
+git push
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+# 将gh-pages的内容直接部署到对应地址
+git subtree push --prefix dist origin gh-pages
 
 # 如果你要部署在 https://<USERNAME>.github.io
-git push -f git@github.com:XAihan/XAihan.github.io.git main
+# git push -f git@github.com:XAihan/XAihan.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/scrollerNumber/',
   server: {
     open: '/',
     // host: '127.0.0.1'
     // host: '10.205.96.245'
   },
+  publicDir:'',
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
